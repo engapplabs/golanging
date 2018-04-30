43c5e3c37bd6e0f523df42454b7e0fddccfe1cd3
ABuarque <abuarquemf@gmail.com>
2018-04-30 19:42:15 -0300

    using vars

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var name = "Aurelio"
+	fmt.Println(name)
+
+	var var1, var2 int = 1,2
+	fmt.Println(var1, var2)
+
+	someValue := "vaca"
+	fmt.Println(someValue)
+}
