6d018374f5d9212b8df326682be588b32615c9dd
ABuarque <abuarquemf@gmail.com>
2018-04-30 19:48:31 -0300

    fors

---
+++ for.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+	//like a while
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	//like c++ for
+	for i := 0; i < 10; i++ {
+		fmt.Println(i * 2)
+	}
+
+	
+
+}
